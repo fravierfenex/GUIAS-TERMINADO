93ac6e00fa196298b8d836f8aac3dbf613ef80a3
fravierfenex <47570880+fravierfenex@users.noreply.github.com>
2019-04-11 23:27:45 -0600

    claseyobjeto
    claseyobjeto

---
+++ claseyobjeto.vb
@@ -0,0 +1,16 @@
+﻿Public Class carro
+
+    Public marca As String
+    Public modelo As String
+    Public año As Integer
+    Public color As String
+    Public Sub car()
+        Console.WriteLine("marca: " & marca)
+        Console.WriteLine("modelo: " & modelo)
+        Console.WriteLine("año: " & año)
+        Console.WriteLine("color: " & color)
+
+    End Sub
+
+
+End Class
