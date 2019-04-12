f623c7b9367cc3a6546272da95ad58879b472fa2
fravierfenex <villafuertearevalo1@gmail.com>
2019-04-11 23:09:05 -0600

    calculadora

---
+++ calculadorasimple.vb
@@ -0,0 +1,55 @@
+﻿Module Module1
+
+    Sub Main()
+        'Dim hola As CuentaBancaria = New CuentaBancaria(1, 10.5)
+        'hola.Depositar(500)
+        'hola.Depositar(1000)
+        'hola.Retirar(800)
+
+        'hola.MostrarBalance()
+
+        Dim opc As Char
+        Dim calcu As Calculadora = New Calculadora()
+        Dim a, b As Double
+
+        'Solicitar operación a realizar
+
+        Console.WriteLine("¿Qué operación deseas realizar? Presiona la letra que corresponda y luego Enter.")
+        Console.WriteLine("[s]umar, [r]estar, [m]ultiplicar, [d]ividir, [t]odas")
+        opc = Console.ReadLine()
+
+        Console.WriteLine("Ingrese el primer valor: ")
+        a = Console.ReadLine()
+        Console.WriteLine("Ingrese el segundo valor: ")
+        b = Console.ReadLine()
+
+        Select Case opc
+            Case "s"
+                Console.Write("La suma de esos números es: ")
+                calcu.Sumar(a, b)
+
+            Case "r"
+                Console.Write("La resta de esos números es: ")
+                calcu.Restar(a, b)
+
+            Case "m"
+                Console.Write("La multiplicacion de esos números es: ")
+                calcu.Multiplicar(a, b)
+
+            Case "d"
+                Console.Write("La division de esos números es: ")
+                calcu.Dividir(a, b)
+
+            Case "t"
+                Console.WriteLine("el resultado de todas las operaciones es: ")
+                calcu.Todas(a, b)
+
+            Case Else
+                Console.WriteLine("OPCION INVALIDA")
+        End Select
+
+        Console.WriteLine("Presiona Enter para salir del programa...")
+        Console.ReadLine()
+    End Sub
+
+End Module
